f2fd0d9a90ce801237681f5f664930bc6b117471
paaatrrrick <patrick.123.foster@gmail.com>
2024-03-26 12:34:54 -0500

    Add login functionality to app.py

--- python/app.py
+++ python/app.py
@@ -4,15 +4,75 @@ import json
 import pyautogui
 
 
-endpoint = "ws://localhost:4500"
+IS_LOCAL = True
+
+
+local_endpoint = "ws://localhost:4500"
+prod_endpoint = "ws://localhost:4500"
+
+endpoint = local_endpoint if IS_LOCAL else prod_endpoint
+
+
+
+
+
+def login(registration, username, password):
+        time.sleep(2)
+        pyautogui.click(1092, 661, button="right")
+        time.sleep(2)
+        pyautogui.click(1208, 352)
+        time.sleep(2)
+        pyautogui.click(367, 16)
+        time.sleep(2)
+        pyautogui.click(1075, 485)
+        time.sleep(2)
+        pyautogui.click(1195, 455)
+        time.sleep(2)
+        pyautogui.typewrite(registration)
+        time.sleep(2)
+        pyautogui.press('enter')
+        time.sleep(2)
+        pyautogui.click(1060, 514)
+        time.sleep(2)
+        pyautogui.click(1078, 440)
+        time.sleep(2)
+        pyautogui.tripleClick(708, 162)
+        time.sleep(2)
+        pyautogui.press('backspace')
+        time.sleep(2)
+        pyautogui.typewrite(username)
+        time.sleep(2)
+        pyautogui.click(705, 204)
+        time.sleep(2)
+        pyautogui.typewrite(password)
+        time.sleep(2)
+        pyautogui.click(731, 262)
+        time.sleep(4)
+        pyautogui.click(585, 310) # click not now for saving password
+        time.sleep(2) # wait much longer for startup script to finish
+        # close tab
+        # maybe just do the whole startup? No, that doesn't scale -- logging in like this may already be unscalable
+        # Goal is to launch.
+
+
+
+
+
 
 
 def on_message(ws, message):
     print("Received from server: " + message)
-
+    try:
+        jsonParsed = json.loads(message)
+        #json parsed should include registration, username, password
+        if 'registration' in jsonParsed and 'username' in jsonParsed and 'password' in jsonParsed:
+            login(jsonParsed['registration'], jsonParsed['username'], jsonParsed['password'])
+        else:
+            print("Error: Missing registration, username, or password")
+            print(jsonParsed)
+    except Exception as e:
+        print(f"Error: {e}")
     #parse json from message
-    jsonParsed = json.loads(message)
-    print(jsonParsed)
 
 
 def on_error(ws, error):
@@ -28,7 +88,7 @@ def on_close(ws, close_status_code, close_msg):
         time.sleep(reconnect_delay)
         try:
             # Attempt to create a new connection
-            ws = websocket.WebSocketApp("ws://localhost:4500",
+            ws = websocket.WebSocketApp(endpoint,
                                          on_message=on_message,
                                          on_error=on_error,
                                          on_close=on_close)
@@ -47,3 +107,9 @@ if __name__ == "__main__":
                                 on_close=on_close)
     # ws.on_open = on_open
     ws.run_forever()
+
+
+
+
+
+

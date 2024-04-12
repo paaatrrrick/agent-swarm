28e4a5470522c4f2d625722cfca14286b0de2652
paaatrrrick <patrick.123.foster@gmail.com>
2024-04-12 18:28:44 -0500

    mobile friendly

--- client/src/app/(unprotected)/page.tsx
+++ client/src/app/(unprotected)/page.tsx
@@ -23,21 +23,24 @@ export default function Page() {
 
   return (
     <div className='w-full h-full bg-primary dark:bg-primary-foreground'>
-      <BackgroundBeams />
+      <div className='hidden lg:flex'>
+        <BackgroundBeams />
+      </div>
+
       {/*absolutely position to center a div */}
       <div
-        className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 flex flex-col items-start justify-start gap-8 w-[1000px]"
+        className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 flex flex-col items-start justify-start gap-8 w-[95vw] lg:w-[1000px]"
       >
-        <h1 className='font-extrabold	text-white text-6xl'>Benchmarking AI Vision Agents</h1>
+        <h1 className='font-extrabold	text-white text-4xl lg:text-6xl'>Benchmarking AI Vision Agents</h1>
         <p className='text-white text-lg font-mono'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore ullam, neque eum accusantium iste hic ea voluptate iure nostrum commodi libero, architecto blanditiis incidunt corrupti esse dolorum, corporis dignissimos magnam?</p>
-        <div className="flex">
+        <div className="flex flex-col items-start gap-4 lg:flex-row lg:gap-0">
           <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none " onClick={() => { window.location.href = constants.routes.dashboard }}>
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-primary dark:bg-primary-foreground px-8 py-1 text-md font-lg text-white backdrop-blur-3xl font-mono">
               Get in touch
             </span>
           </button>
-          <button className="bg-secondary border border-border rounded-full h-12 ml-4 px-3 w-sm flex items-center justify-start hover:cursor-pointer hover:bg-background" onClick={() => { SignUpWithGooglePopUp(() => { }) }}>
+          <button className="bg-primary dark:bg-primary-foreground border border-border rounded-full h-12 lg:ml-4 px-3 w-sm flex items-center justify-start hover:cursor-pointer hover:bg-background" onClick={() => { SignUpWithGooglePopUp(() => { }) }}>
             <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=" />
             <p className='text-sm ml-2 font-mono'>Join with Google</p>
           </button>

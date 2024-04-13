28a2546356ab025cf7e35d4bfaed6f98ff81d51e
paaatrrrick <patrick.123.foster@gmail.com>
2024-04-13 12:45:37 -0500

    Refactor Videoplayer component and adjust delay in BackgroundBeams

--- client/src/components/home/Videoplayer.tsx
+++ client/src/components/home/Videoplayer.tsx
@@ -1,13 +1,10 @@
 //@ts-nocheck
-
 import React from 'react'
 import Videojs from './Videojs';
 
 // This imports the functional component from the previous sample.
-
-const Videoplayer = ({ path }: { path: string }) => {
+const Videoplayer = React.memo(({ path }) => {
     const playerRef = React.useRef(null);
-    //live stream video from path http://45.56.104.109/hls/teststream.m3u8
     const videoJsOptions = {
         autoplay: true,
         controls: true,
@@ -17,7 +14,7 @@ const Videoplayer = ({ path }: { path: string }) => {
             src: path,
             type: 'application/x-mpegURL'
         }]
-    }
+    };
 
     const handlePlayerReady = (player) => {
         playerRef.current = player;
@@ -33,7 +30,44 @@ const Videoplayer = ({ path }: { path: string }) => {
     };
 
     return <Videojs options={videoJsOptions} onReady={handlePlayerReady} />;
-}
+}, (prevProps, nextProps) => prevProps.path === nextProps.path);
 
 
 export default Videoplayer;
+
+// import React from 'react'
+// import Videojs from './Videojs';
+
+// // This imports the functional component from the previous sample.
+
+// const Videoplayer = ({ path }: { path: string }) => {
+//     const playerRef = React.useRef(null);
+//     //live stream video from path http://45.56.104.109/hls/teststream.m3u8
+//     const videoJsOptions = {
+//         autoplay: true,
+//         controls: true,
+//         responsive: true,
+//         fluid: true,
+//         sources: [{
+//             src: path,
+//             type: 'application/x-mpegURL'
+//         }]
+//     }
+
+//     const handlePlayerReady = (player) => {
+//         playerRef.current = player;
+
+//         // You can handle player events here, for example:
+//         player.on('waiting', () => {
+//             videojs.log('player is waiting');
+//         });
+
+//         player.on('dispose', () => {
+//             videojs.log('player will dispose');
+//         });
+//     };
+
+//     return <Videojs options={videoJsOptions} onReady={handlePlayerReady} />;
+// }
+
+// export default Videoplayer;
